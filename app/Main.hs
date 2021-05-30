80ffc979e3eb688df8808cafbeb2ed830fc38caa
Christophe Thibaut <cthibaut@octo.com>
2021-05-30 10:33:18 +0200

    added static style sheet

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,33 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 import           Yesod
+import           Yesod.Static
 import           System.Environment     (getEnv)
 
-data HelloWorld = HelloWorld
+data HelloWorld = HelloWorld { getStatic :: Static }
+
+staticFiles "static"
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/static StaticR Static getStatic
 |]
 
 instance Yesod HelloWorld
 
+
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|
-        <h1>Hello World!
-        <p>Example page by ToF
+getHomeR = do
+    defaultLayout $ do
+        addStylesheet $ StaticR css_main_css
+        [whamlet|
+            <h1>Hello World!
+            <p>Example page by ToF
         |]
 
 main :: IO ()
 main = do
     port <- read <$> getEnv "PORT"
-    warp port HelloWorld
+    -- Get the static subsite, as well as the settings it is based on
+    static@(Static settings) <- staticDevel "static"
+    warp port (HelloWorld static)
