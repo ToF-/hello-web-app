5d108c32c990b9bde9208ec7185b0ce758936695
Christophe Thibaut <cthibaut@octo.com>
2021-05-30 13:24:55 +0200

    added segmentdisplay

--- app/Main.hs
+++ app/Main.hs
@@ -21,10 +21,17 @@ instance Yesod HelloWorld
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
+        addScript $ StaticR js_segmentdisplay_js
+        addScript $ StaticR js_initdisplay_js
         addStylesheet $ StaticR css_main_css
         [whamlet|
-            <h1>Hello World!
-            <p>Example page by ToF
+            <body onload=initDisplay('-17.2')>
+                <h1>Hello World!
+                <p>Example page by ToF
+                <div style="padding: 50px 100px">
+                      <div style="width: 216px; height: 126px; position: relative; background: transparent url(static/images/plexiglas.png) no-repeat top left">
+                            <div style="position: absolute; left: 38px; top: 33px; width: 120px; height: 34px">
+                              <canvas id="display" width="120" height="34">
         |]
 
 main :: IO ()
@@ -33,3 +40,4 @@ main = do
     -- Get the static subsite, as well as the settings it is based on
     static@(Static settings) <- staticDevel "static"
     warp port (HelloWorld static)
+
---
+++ static/js/initdisplay.js
@@ -0,0 +1,6 @@
+var display;
+function initDisplay(value) {
+    display=new SegmentDisplay("display");
+    display.setValue(value);
+    display.draw();
+};
